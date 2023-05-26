26010f22e20b6ad25843f250c6f3fb0f2d4d8afc
DiangoGav <diangogavidia@gmail.com>
2023-05-26 14:41:38 -0400

    Extract location to a class in Driver entity

--- src/modules/driver/domain/Driver.ts
+++ src/modules/driver/domain/Driver.ts
@@ -1,13 +1,12 @@
+import { Location } from "../../shared/location/domain/Location";
+
 export class Driver {
   readonly id: string;
   readonly name: string;
   readonly isAvailable: boolean;
-  readonly location: {
-    latitude: number;
-    longitude: number;
-  }
+  readonly location: Location
 
-  constructor({ id, name, isAvailable, location }: { id: string; name: string; isAvailable: boolean; location: { latitude: number; longitude: number } }) {
+  constructor({ id, name, isAvailable, location }: { id: string; name: string; isAvailable: boolean; location: Location }) {
     this.id = id;
     this.name = name;
     this.isAvailable = isAvailable;
--- src/modules/driver/infrastructure/mongodb/DriverMongooseRepository.ts
+++ src/modules/driver/infrastructure/mongodb/DriverMongooseRepository.ts
@@ -1,4 +1,5 @@
 import { Criteria } from "../../../shared/criteria/domain/Criteria";
+import { Location } from "../../../shared/location/domain/Location";
 import { Driver } from "../../domain/Driver";
 import { DriverFilter } from "../../domain/DriverGetterFilter";
 import { DriverRepository } from "../../domain/DriverRepository";
@@ -11,10 +12,10 @@ export class DriverMongooseRepository implements DriverRepository {
       const data = await DriverModel.find(filter).limit(criteria.limit).lean();
       return data.map((item) => new Driver({
         ...item,
-        location: {
+        location: new Location({
           latitude: item.coordinates[1],
           longitude: item.coordinates[0]
-        }
+        })
       }))
     }
 
@@ -47,20 +48,20 @@ export class DriverMongooseRepository implements DriverRepository {
     if (!data) { return null }
     return new Driver({
       ...data,
-      location: {
+      location: new Location({
         latitude: data.coordinates[1],
         longitude: data.coordinates[0]
-      }
+      })
     });
   }
   async get(): Promise<Driver[]> {
     const data = await DriverModel.find().lean();
     return data.map((item) => new Driver({
       ...item,
-      location: {
+      location: new Location({
         latitude: item.coordinates[1],
         longitude: item.coordinates[0]
-      }
+      })
     }))
   }
 }
