27c7cfb973baaeb7452ad8b56f07af195d519a77
DiangoGav <diangogavidia@gmail.com>
2023-05-29 19:14:42 -0400

    jest config for unit tests

---
+++ jest.config.units.ts
@@ -0,0 +1,17 @@
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+export default {
+  clearMocks: true,
+  preset: "ts-jest",
+  roots: [
+    "<rootDir>/tests",
+  ],
+  testEnvironment: "node",
+  verbose: true,
+  transform: {
+    '^.+\\.ts?$': ['@swc/jest']
+  },
+};
